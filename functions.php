64bd9691946e3f92d47940f138715c67913f14da
MagNadAnn <MagNadAnn@users.noreply.github.com>
2020-12-18 17:56:56 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,79 @@
+<?php
+/**
+** activation theme
+**/
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+}
+
+// ajouter une nouvelle zone de menu à mon thème
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'footer' => __( 'Footer' ),
+        )
+    );
+}
+add_action( 'init', 'register_my_menus' );
+
+class CSS_Menu_WalkerFooter extends Walker {
+
+	var $db_fields = array('parent' => 'menu_item_parent', 'id' => 'db_id');
+	
+	function start_lvl(&$output, $depth = 0, $args = array()) {
+		$indent = str_repeat("\t", $depth);
+		$output .= '<ul>';
+	}
+	
+	function end_lvl(&$output, $depth = 0, $args = array()) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent</ul>\n";
+	}
+	
+	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+	
+		global $wp_query;
+		$indent = ($depth) ? str_repeat("\t", $depth) : '';
+		$class_names = $value = '';
+		$classes = empty($item->classes) ? array() : (array) $item->classes;
+		
+		/* Add active class */
+		if (in_array('current-menu-item', $classes)) {
+			$classes[] = 'active';
+			unset($classes['current-menu-item']);
+		}
+		
+		/* Check for children */
+		$children = get_posts(array('post_type' => 'nav_menu_item', 'nopaging' => true, 'numberposts' => 1, 'meta_key' => '_menu_item_menu_item_parent', 'meta_value' => $item->ID));
+		if (!empty($children)) {
+			$classes[] = 'has-sub';
+		}
+		
+		$class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
+		$class_names = $class_names ? ' class="footer-site__links_item ' . esc_attr($class_names) . '"' : '';
+		
+		$id = apply_filters('nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args);
+		$id = $id ? ' id="' . esc_attr($id) . '"' : '';
+		
+		$output .= $indent . '<li' . $id . $value . $class_names .'>';
+		
+		$attributes  = ! empty($item->attr_title) ? ' title="'  . esc_attr($item->attr_title) .'"' : '';
+		$attributes .= ! empty($item->target)     ? ' target="' . esc_attr($item->target    ) .'"' : '';
+		$attributes .= ! empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn       ) .'"' : '';
+        $attributes .= ! empty($item->url)        ? ' href="'   . esc_attr($item->url       ) .'"' : '';
+		
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .' class="footer-site__link">';
+		$item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+		
+		$output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+	}
+	
+	function end_el(&$output, $item, $depth = 0, $args = array()) {
+		$output .= "</li>\n";
+	}
+}
